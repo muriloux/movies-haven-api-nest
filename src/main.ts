9efd0e737ac31a6c385a77ac1f7419abce744c76
muriloux <murilomelo.code@gmail.com>
2023-07-24 09:33:20 -0300

    add updateMovie

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(port, () => {
     console.log(`[server] Listening on  port ${port}`);
   });
